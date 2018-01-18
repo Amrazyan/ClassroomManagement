f12194340aff054f51ee4191f4551038220a28ce
Arman Amrazyan <amrazyan97@outlook.com>
2018-01-18 12:13:39 +0400

    Importing project

---
+++ Diplom2017/Controllers/UserPageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diplom2017.Controllers
+{
+    public class UserPageController : Controller
+    {
+        // GET: UserPage
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name, string password)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                ModelState.AddModelError("Error", "Wrong name or password");
+            }
+            else
+            {
+               
+                return View("User");
+            }
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Diplom2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Diplom2017.MvcApplication" Language="C#" %>
